c9df24e7905e4e557d6e49316a5e270cdbd4def5
Dong H. Kim <ehdgus1524@gmail.com>
2022-10-11 17:28:50 +0900

    [ADD] category Parsing

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
-import { parseRegion, parseUrl } from "./libraries/parse";
+import { parseCategory, parseRegion, parseUrl } from "./libraries/parse";
 
 // await parseRegion();
 // // await parseUrl();
+await parseCategory();
--- src/libraries/parse.ts
+++ src/libraries/parse.ts
@@ -1,6 +1,7 @@
 import { getHtml } from "./getHtml";
 import { text, load } from "cheerio";
 import { Logger } from "utils/logger.utils";
+import axios from "axios";
 
 export async function parseRegion() {
   const regionArray: Array<string> = [];
@@ -39,19 +40,46 @@ export async function parseUrl() {
     return console.log("No Html");
   }
 
-  const $ = load(html.data);
+  try {
+    const $ = load(html.data);
 
-  const $href = $(".card-link");
+    const $href = $(".card-link");
 
-  const url = $href.map((index, data) => {
-    const href = data.attribs.href.split("=");
-    const link = href[href.length - 1];
+    const url = $href.map((index, data) => {
+      const href = data.attribs.href.split("=");
+      const link = href[href.length - 1];
 
-    urlArray.push("https://www.daangn.com/hot_articles" + link);
+      Logger.info(`[URL_SCRAPER] https://www.daangn.com/hot_articles${link}`);
+      urlArray.push("https://www.daangn.com/hot_articles" + link);
+    });
+
+    return urlArray;
+  } catch (error) {
+    if (error instanceof Error) {
+      throw new Error("URL_SCRAPING FAILED");
+    }
+  }
+}
+
+export async function parseCategory() {
+  const categoryArray: Array<string> = [];
+
+  const url = await parseUrl();
+
+  if (!url) {
+    return Logger.debug("[CATEGORY] No Url Found Here. Ignored");
+  }
+
+  const category = url.find(async (uri) => {
+    const html = await axios.get(uri);
+    const $ = load(html.data);
+
+    const category = $(".article-category").text();
+
+    Logger.info("[CATEGORY] Category Found");
+
+    categoryArray.push(category);
   });
 
-  Logger.info(urlArray);
-  console.log(urlArray);
-
-  return urlArray;
+  return categoryArray;
 }
