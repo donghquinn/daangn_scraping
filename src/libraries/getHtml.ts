318cf40a664a32a2c51744e9ee68def11ec3015f
Dong H. Kim <ehdgus1524@gmail.com>
2022-10-11 19:56:25 +0900

    [CHANGED] Fetch Html Error Handling

--- src/libraries/getHtml.ts
+++ src/libraries/getHtml.ts
@@ -6,6 +6,8 @@ export async function getHtml() {
       "https://www.daangn.com/region/%EC%84%9C%EC%9A%B8%ED%8A%B9%EB%B3%84%EC%8B%9C"
     );
   } catch (error) {
-    console.error(error);
+    if (error instanceof Error) {
+      throw new Error(`[FETCH] ${error.message}`);
+    }
   }
 }
