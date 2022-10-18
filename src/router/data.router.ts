787a844d783dec714b07ea3bbaaddfc576a79385
Dong H. Kim <ehdgus1524@gmail.com>
2022-10-18 17:38:28 +0900

    [CHANGED] router Record

--- src/router/data.router.ts
+++ src/router/data.router.ts
@@ -1,10 +1,11 @@
 import { Next } from "koa";
 import Router from "koa-router";
 import { DataAnalyze } from "libraries/analyze/split";
+import { GetCombined } from "types/sql.types";
 
 const { getAllData, getTotalCount } = DataAnalyze.getInstance();
 
-const dataRouter = new Router<Record<string, never>>();
+const dataRouter = new Router<Record<string, Array<GetCombined[]>>>();
 
 dataRouter.get("/data", async (ctx, next: Next) => {
   ctx.body = await getAllData();
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,7 +1,10 @@
 import Router from "koa-router";
+import { GetCombined } from "types/sql.types";
 import { dataRouter } from "./data.router";
 
-const routerV1 = new Router<Record<string, never>>({ prefix: "/api" });
+const routerV1 = new Router<Record<string, Array<GetCombined[]>>>({
+  prefix: "/api",
+});
 
 routerV1.use(dataRouter.routes());
 
