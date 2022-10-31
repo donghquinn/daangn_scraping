742f51f5d48380e3f304b18b62063b9388def7ef
Dong H. Kim <ehdgus1524@gmail.com>
2022-10-31 11:59:40 +0900

    [CHANGED] router prefix

--- src/router/v2/index.ts
+++ src/router/v2/index.ts
@@ -2,7 +2,7 @@ import { Context } from "koa";
 import Router from "koa-router";
 import { regionScoreRouter } from "./score.router";
 
-const routerV2 = new Router<Record<string, Context>>();
+const routerV2 = new Router<Record<string, Context>>({ prefix: "/v2" });
 
 routerV2.use(regionScoreRouter.routes());
 
