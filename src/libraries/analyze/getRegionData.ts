7e4b4e5be455578649434581a12a1e2a0bc33689
Dong H. Kim <ehdgus1524@gmail.com>
2022-10-31 10:46:04 +0900

    [CHANGED] analyze getRegionData -> dynamic obj

--- src/libraries/analyze/getRegionData.ts
+++ src/libraries/analyze/getRegionData.ts
@@ -2,17 +2,20 @@ import { Context } from "koa";
 import { Mysql } from "libraries/database";
 import { setErrorResponse } from "libraries/request.lib";
 import { selectRegion } from "queries/select-data";
+import { RegionScoreObject } from "types/bestRegion.types";
 import { GetRegion } from "types/sql.types";
 
 export async function getAllRegionList(ctx: Context) {
-  // 전체 데이터 지역 정리 - 쿼리 결과 그대로
+  // 전체 데이터 지역 정리 - 쿼리 결과에서 두번째 지역 정보만 넣은 그대로
   const totalArray: string[] = [];
 
   // 지역 리스트
   const regionArray: string[] = [];
 
+  const returnData: RegionScoreObject = {};
+
   try {
-    const [region] = await Mysql.query<GetRegion[]>(selectRegion);
+    const region = await Mysql.query<GetRegion[]>(selectRegion);
 
     for (let regions in region) {
       const splittedRegion = regions.split(" ")[1];
@@ -24,10 +27,18 @@ export async function getAllRegionList(ctx: Context) {
         return;
       }
 
+      // 만약 해당 지역이 이미 객체에 등록되어 있을 경우 점수 1 추가
+      if (returnData[regions]) {
+        returnData[regions] + 1;
+      }
+
+      // 없을 경우 새롭게 등록
+      returnData[regions] = 0;
+
       regionArray.push(splittedRegion);
     }
 
-    // 이제 totalArray의 각 지역들을 가지고 regionArray에 있는 각각의 지역들에 카운트
+    // TODO 위에서 동적으로 생성한 객체의 value값을 기준으로 내림차순...
   } catch (error) {
     setErrorResponse(ctx, 500, "[Get Best Region Error Found]");
   }
---
+++ src/types/bestRegion.types.ts
@@ -0,0 +1,4 @@
+// 지역 정보 쿼리 후 동적 객체 선언할 때의 키:값 타입 지정
+export interface RegionScoreObject {
+  [key: string]: number;
+}
