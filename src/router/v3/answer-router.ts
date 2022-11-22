aeb7c0e419d3172ae9660b63eca26d65c52dea55
Dong H. Kim <ehdgus1524@gmail.com>
2022-11-23 01:23:27 +0900

    [REMOVED] v3 on v3router prefix

--- src/router/v3/answer-router.ts
+++ src/router/v3/answer-router.ts
@@ -2,9 +2,7 @@ import { DefaultContext, DefaultState, Next } from "koa";
 import Router from "koa-router";
 import { surveyControl } from "libraries/survey.lib";
 
-const surveyRouter = new Router<DefaultState, DefaultContext>({
-  prefix: "/v3",
-});
+const surveyRouter = new Router<DefaultState, DefaultContext>();
 
 surveyRouter.post("/answer", async (ctx, next: Next) => {
   await surveyControl(ctx);
