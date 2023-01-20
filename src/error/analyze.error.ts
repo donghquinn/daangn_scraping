a5d112807ebb8ed792a1d3b34b21fab50ecde1f6
Kim Donghyun <kimdonghyun@Kimui-MacBookAir.local>
2023-01-21 02:49:40 +0900

    [ADDED] Analyze Error

--- src/error/analyze.error.ts
+++ src/error/analyze.error.ts
@@ -1,11 +1,11 @@
-export class SurveyError extends Error {
+export class AnalyzeError extends Error {
   type: string;
 
   constructor(type: string, message: string, cause?: Error) {
     super(message);
 
     this.type = type;
-    this.name = "[SURVEY]";
+    this.name = "[ANALYZE]";
     this.cause = cause;
   }
 }
