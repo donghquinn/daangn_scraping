b788d4fc147168783585a09a65d5bf12c8617946
Kim Donghyun <kimdonghyun@Kimui-MacBookAir.local>
2023-02-15 11:57:02 +0900

    [CHANGED] removed logging

--- src/libraries/analyze/split.ts
+++ src/libraries/analyze/split.ts
@@ -1,12 +1,5 @@
-// TODO 카테고리, 지역 이름 데이터 뽑아와서 해당 타입 가드하고
-// TODO find() 함수로 item.region = "동작구" 식으로 아이템 뽑아와서 총 개수
-
-import { CommonLogger } from "utils/logger.utils";
-
 export function splitDate(updated: string) {
   const date = updated.split(" ");
-
-  CommonLogger.info(`date Split`);
 
   return date;
 }
